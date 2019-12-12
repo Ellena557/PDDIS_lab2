dd8c8dd9d189a28dfa5dddc001ff0c22b1e6ef21
Elena Gorskaya <helena557@mail.ru>
2019-12-12 15:47:49 +0300

    Update WTemperature.java

--- src/main/java/hello/JsonParsing/WTemperature.java
+++ src/main/java/hello/JsonParsing/WTemperature.java
@@ -6,7 +6,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 //@JsonIgnoreProperties({"latitude", "longtitude"})
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WTemperature {
+    public TempOneDay getHourly() {
+        return hourly;
+    }
+
+    public void setHourly(TempOneDay hourly) {
+        this.hourly = hourly;
+    }
+
     //@JsonProperty("temperature")
     //public Double temperature;
-    public TempOneDay hourly;
+    private TempOneDay hourly;
 }
