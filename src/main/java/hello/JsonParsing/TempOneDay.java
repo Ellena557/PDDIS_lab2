01333a7761af6b792bea0ff3ed9ecadcb558d8c3
Elena Gorskaya <helena557@mail.ru>
2019-12-12 15:48:45 +0300

    Update TempOneDay.java

--- src/main/java/hello/JsonParsing/TempOneDay.java
+++ src/main/java/hello/JsonParsing/TempOneDay.java
@@ -6,5 +6,13 @@ import java.util.ArrayList;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class TempOneDay {
-    public ArrayList<TempOneDayData> data;
+    public ArrayList<TempOneDayData> getData() {
+        return data;
+    }
+
+    public void setData(ArrayList<TempOneDayData> data) {
+        this.data = data;
+    }
+
+    private ArrayList<TempOneDayData> data;
 }
